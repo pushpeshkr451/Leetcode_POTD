358a9b668a34446fd78ac5cede86e59dc664c827
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-06-09 19:42:19 +0530

    Create 4305-CheckAdjacentDigitDifferences

---
+++ 4305-CheckAdjacentDigitDifferences/4305-CheckAdjacentDigitDifferences.cpp
@@ -0,0 +1,17 @@
+// Last updated: 09/06/2026, 19:42:19
+class Solution {
+public:
+    bool isAdjacentDiffAtMostTwo(string s) {
+        int n=s.size();
+
+        for(int i=0;i<n-1;i++){
+            int a=s[i]-'0';
+            int b=s[i+1]-'0';
+
+            if(abs(a-b)>2)return false;
+        }
+
+        return true;
+        
+    }
+};
