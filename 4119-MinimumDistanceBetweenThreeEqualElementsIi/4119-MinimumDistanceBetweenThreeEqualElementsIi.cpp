2c8ea75f0d842c63852f2de201d5d3af992fe9d8
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-06-09 19:43:59 +0530

    Create 4119-MinimumDistanceBetweenThreeEqualElementsIi

---
+++ 4119-MinimumDistanceBetweenThreeEqualElementsIi/4119-MinimumDistanceBetweenThreeEqualElementsIi.cpp
@@ -0,0 +1,19 @@
+// Last updated: 09/06/2026, 19:43:59
+class Solution {
+public:
+    int minimumDistance(vector<int>& nums) {
+        unordered_map<int, vector<int>> mp;
+        for (int i = 0; i < (int)nums.size(); ++i) mp[nums[i]].push_back(i);
+        
+        int ans = INT_MAX;
+        for (auto &pr : mp) {
+            auto &pos = pr.second;
+            if (pos.size() < 3) continue;
+            for (int i = 0; i + 2 < (int)pos.size(); ++i) {
+                int dist = 2 * (pos[i+2] - pos[i]); 
+                if (dist < ans) ans = dist;
+            }
+        }
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
