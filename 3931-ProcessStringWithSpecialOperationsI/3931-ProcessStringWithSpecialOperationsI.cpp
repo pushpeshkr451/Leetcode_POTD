3cdac08a37df6ee1f2b1373a24e0ecc4809b2dfb
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-06-09 19:45:17 +0530

    Create 3931-ProcessStringWithSpecialOperationsI

---
+++ 3931-ProcessStringWithSpecialOperationsI/3931-ProcessStringWithSpecialOperationsI.cpp
@@ -0,0 +1,19 @@
+// Last updated: 09/06/2026, 19:45:16
+class Solution {
+public:
+    string processStr(std::string s) {
+        string res = "";
+        for (char c : s) {
+            if (c != '*' && c != '#' && c != '%') {
+                res += c;
+            } else if (c == '#') {
+                res += res;
+            } else if (c == '%') {
+                reverse(res.begin(), res.end());
+            } else {
+                if (!res.empty()) res.pop_back();
+            }
+        }
+        return res;
+    }
+};
