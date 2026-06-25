c0af0cfe9568641389394bc321f7dc4af26b0794
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-06-25 12:28:55 +0530

    Create 3737-CountSubarraysWithMajorityElementI

---
+++ 3737-CountSubarraysWithMajorityElementI/version/cpp/3737-CountSubarraysWithMajorityElementI_20260625_122854.cpp
@@ -0,0 +1,18 @@
+// Last updated: 25/06/2026, 12:28:54
+1class Solution {
+2public:
+3    int countMajoritySubarrays(vector<int>& nums, int target) {
+4        int n = nums.size();
+5        int ans = 0;
+6        for (int i = 0; i < n; ++i) {
+7            int cnt = 0;
+8            for (int j = i; j < n; ++j) {
+9                cnt += (nums[j] == target ? 1 : -1);
+10                if (cnt > 0) {
+11                    ++ans;
+12                }
+13            }
+14        }
+15        return ans;
+16    }
+17};
