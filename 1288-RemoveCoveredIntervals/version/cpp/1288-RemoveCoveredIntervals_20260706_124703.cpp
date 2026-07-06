d634a482bb37c5cb953b6677169a7d8b26cc96cf
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-07-06 12:47:03 +0530

    Create 1288-RemoveCoveredIntervals

---
+++ 1288-RemoveCoveredIntervals/version/cpp/1288-RemoveCoveredIntervals_20260706_124703.cpp
@@ -0,0 +1,22 @@
+// Last updated: 06/07/2026, 12:47:03
+1class Solution {
+2public:
+3    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+4        sort(intervals.begin(), intervals.end(), [](auto &a, auto &b) {
+5            if (a[0] == b[0]) return a[1] > b[1];
+6            return a[0] < b[0];
+7        });
+8
+9        int ans = 0;
+10        int prevEnd = -1;
+11
+12        for (auto &it : intervals) {
+13            if (it[1] > prevEnd) {
+14                ans++;
+15                prevEnd = it[1];
+16            }
+17        }
+18
+19        return ans;
+20    }
+21};
