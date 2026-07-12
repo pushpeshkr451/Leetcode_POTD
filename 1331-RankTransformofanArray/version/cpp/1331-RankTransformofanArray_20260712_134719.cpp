2ddfafca867fdeb17a66bc5de34eb50dc7dee8d5
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-07-12 13:47:19 +0530

    Create 1331-RankTransformofanArray

---
+++ 1331-RankTransformofanArray/version/cpp/1331-RankTransformofanArray_20260712_134719.cpp
@@ -0,0 +1,18 @@
+// Last updated: 12/07/2026, 13:47:19
+1class Solution {
+2public:
+3    vector<int> arrayRankTransform(vector<int>& arr) {
+4        set<int>st(arr.begin(),arr.end());
+5        vector<int>nums(st.begin(),st.end());
+6
+7        sort(nums.begin(),nums.end());
+8        unordered_map<int,int>mp;
+9
+10        for(int i=0;i<nums.size();i++)mp[nums[i]]=i+1;
+11
+12        for(int i=0;i<arr.size();i++)arr[i]=mp[arr[i]];
+13
+14        return arr;
+15
+16    }
+17};
