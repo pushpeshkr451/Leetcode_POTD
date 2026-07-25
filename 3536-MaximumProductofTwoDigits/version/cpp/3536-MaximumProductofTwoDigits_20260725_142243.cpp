304d9092e7cc8ed313de01e4e27ffca9689846f5
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-07-25 14:22:44 +0530

    Create 3536-MaximumProductofTwoDigits

---
+++ 3536-MaximumProductofTwoDigits/version/cpp/3536-MaximumProductofTwoDigits_20260725_142243.cpp
@@ -0,0 +1,10 @@
+// Last updated: 25/07/2026, 14:22:43
+1class Solution {
+2public:
+3    int maxProduct(int n) {
+4        string s=to_string(n);
+5        sort(s.begin(),s.end());
+6        int p=s.size();
+7        return (s[p-1]-'0')*(s[p-2]-'0');
+8    }
+9};
