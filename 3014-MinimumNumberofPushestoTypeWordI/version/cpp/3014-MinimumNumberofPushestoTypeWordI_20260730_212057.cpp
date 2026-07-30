55bfb792e26b6738801c91a514ff84e81b038911
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-07-30 21:20:57 +0530

    Create 3014-MinimumNumberofPushestoTypeWordI

---
+++ 3014-MinimumNumberofPushestoTypeWordI/version/cpp/3014-MinimumNumberofPushestoTypeWordI_20260730_212057.cpp
@@ -0,0 +1,12 @@
+// Last updated: 30/07/2026, 21:20:57
+1class Solution {
+2public:
+3    int minimumPushes(string word) {
+4        int n = word.size();
+5        int ans = 0;
+6        for (int i = 0; i < n; ++i) {
+7            ans += i / 8 + 1;
+8        }
+9        return ans;
+10    }
+11};
