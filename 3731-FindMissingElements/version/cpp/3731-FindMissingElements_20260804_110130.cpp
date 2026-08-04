61e98f553e2681cc953ca4c78464fb8b9a2d8743
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-08-04 11:01:31 +0530

    Create 3731-FindMissingElements

---
+++ 3731-FindMissingElements/version/cpp/3731-FindMissingElements_20260804_110130.cpp
@@ -0,0 +1,18 @@
+// Last updated: 04/08/2026, 11:01:30
+1class Solution {
+2public:
+3    vector<int> findMissingElements(vector<int>& nums) {
+4        vector<int>ans;
+5        int mi=*min_element(nums.begin(),nums.end());
+6        int ma=*max_element(nums.begin(),nums.end());
+7
+8        set<int>st(nums.begin(),nums.end());
+9
+10        for(int i=mi+1;i<ma;i++){
+11            if(st.find(i)==st.end()){
+12                ans.push_back(i);
+13            }
+14        }
+15        return ans;
+16    }
+17};
