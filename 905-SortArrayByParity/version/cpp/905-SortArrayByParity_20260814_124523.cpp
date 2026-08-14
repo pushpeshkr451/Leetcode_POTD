74011c206a823b23481bde492c1bee0560b7556a
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-08-14 12:45:24 +0530

    Create 905-SortArrayByParity

---
+++ 905-SortArrayByParity/version/cpp/905-SortArrayByParity_20260814_124523.cpp
@@ -0,0 +1,17 @@
+// Last updated: 14/08/2026, 12:45:23
+1class Solution {
+2public:
+3    vector<int> sortArrayByParity(vector<int>& nums) {
+4        vector<int>ans;
+5
+6        for(auto it:nums){
+7            if(it%2==0)ans.push_back(it);
+8        }
+9        
+10        for(auto it:nums){
+11            if(it%2)ans.push_back(it);
+12        }
+13
+14        return ans;
+15    }
+16};
