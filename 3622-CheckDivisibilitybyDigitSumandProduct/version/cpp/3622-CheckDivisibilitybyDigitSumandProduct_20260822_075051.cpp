d487aea7ca1efce98410184d49bd8d04d20fbea1
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-08-22 07:50:52 +0530

    Create 3622-CheckDivisibilitybyDigitSumandProduct

---
+++ 3622-CheckDivisibilitybyDigitSumandProduct/version/cpp/3622-CheckDivisibilitybyDigitSumandProduct_20260822_075051.cpp
@@ -0,0 +1,17 @@
+// Last updated: 22/08/2026, 07:50:51
+1class Solution {
+2public:
+3    bool checkDivisibility(int n) {
+4        string s=to_string(n);;
+5
+6        long long sum=0,prod=1;
+7
+8        for(auto it:s){
+9            int num=it-'0';
+10            sum+=num;
+11            prod*=num;
+12        }
+13
+14        return n%(prod+sum)==0?true:false;
+15    }
+16};
