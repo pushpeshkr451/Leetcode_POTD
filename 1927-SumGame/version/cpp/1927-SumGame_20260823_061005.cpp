4c760a1c6c9682c1cca913e86553878737733917
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-08-23 06:10:06 +0530

    Create 1927-SumGame

---
+++ 1927-SumGame/version/cpp/1927-SumGame_20260823_061005.cpp
@@ -0,0 +1,28 @@
+// Last updated: 23/08/2026, 06:10:05
+1class Solution {
+2public:
+3    bool sumGame(string s) {
+4        double res = 0;
+5        double n = s.length();
+6        for (int i = 0; i < n; i++) {
+7            double sign;
+8            if (i < n / 2) {
+9                sign = 1;
+10            } else {
+11                sign = -1;
+12            }
+13            double value;
+14            if (s[i] == '?') {
+15                value = 4.5;
+16            } else {
+17                value = s[i] - '0';
+18            }
+19            res += sign * value;
+20        }
+21        if (res != 0.0) {
+22            return true;
+23        } else {
+24            return false;
+25        }
+26    }
+27};
