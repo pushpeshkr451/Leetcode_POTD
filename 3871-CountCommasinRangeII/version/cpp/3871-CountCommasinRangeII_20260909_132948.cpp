c370f66e51e3ed249ade031108951b6db3c288e6
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-09-09 13:29:49 +0530

    Create 3871-CountCommasinRangeII

---
+++ 3871-CountCommasinRangeII/version/cpp/3871-CountCommasinRangeII_20260909_132948.cpp
@@ -0,0 +1,22 @@
+// Last updated: 09/09/2026, 13:29:48
+1class Solution {
+2public:
+3    long long countCommas(long long n) {
+4        if(n<1000)return 0;
+5
+6        long long ans=0;
+7
+8        for(int d=4;d<=16;d++){
+9            long long l=pow(10,d-1);
+10
+11            if(l>n)break;
+12            long long r=min(n,(long long)pow(10,d)-1);
+13
+14            long long cnt=r-l+1;
+15
+16            long long com=(d-1)/3;
+17            ans+=cnt*com;
+18        }
+19        return ans;
+20    }
+21};
