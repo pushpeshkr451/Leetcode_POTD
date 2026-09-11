a2f7291d72b5bc2c2bd7ebb6474667fb2a3c6479
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-09-11 22:57:26 +0530

    Create 719-FindK-thSmallestPairDistance

---
+++ 719-FindK-thSmallestPairDistance/version/cpp/719-FindK-thSmallestPairDistance_20260911_225726.cpp
@@ -0,0 +1,36 @@
+// Last updated: 11/09/2026, 22:57:26
+1class Solution {
+2public:
+3    int smallestDistancePair(vector<int>& nums, int k) {
+4        sort(nums.begin(), nums.end());
+5
+6        int n = nums.size();
+7
+8        int low = 0;
+9        int high = nums[n - 1] - nums[0];
+10
+11        while(low < high) {
+12            int mid = low + (high - low) / 2;
+13
+14            int left = 0;
+15            int count = 0;
+16
+17            for(int right = 0; right < n; right++) {
+18                while(nums[right] - nums[left] > mid) {
+19                    left++;
+20                }
+21
+22                count += right - left;
+23            }
+24
+25            if(count >= k) {
+26                high = mid;
+27            }
+28            else {
+29                low = mid + 1;
+30            }
+31        }
+32
+33        return low;
+34    }
+35};
