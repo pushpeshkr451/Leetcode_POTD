8762fd4c7d71dc55e1be8c89167de9e8ad2ca6e5
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-09-11 15:01:19 +0530

    Create 3483-Unique3-DigitEvenNumbers

---
+++ 3483-Unique3-DigitEvenNumbers/version/cpp/3483-Unique3-DigitEvenNumbers_20260911_150118.cpp
@@ -0,0 +1,26 @@
+// Last updated: 11/09/2026, 15:01:18
+1class Solution {
+2public:
+3    int totalNumbers(vector<int>& digits) {
+4        set<int> st;
+5        int n = digits.size();
+6        for (int i = 0; i < n; i++) {
+7            if (digits[i] == 0)
+8                continue;
+9            for (int j = 0; j < n; j++) {
+10                if (j == i)
+11                    continue;
+12                for (int k = 0; k < n; k++) {
+13                    if (k == i || k == j)
+14                        continue;
+15                    if (digits[k] % 2 == 1)
+16                        continue;
+17                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+18                    st.insert(num);
+19                }
+20            }
+21        }
+22
+23        return st.size();
+24    }
+25};
