bbad2023c394e0d74515f3e5ccdc078ebff16361
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-09-12 21:13:12 +0530

    Create 948-BagofTokens

---
+++ 948-BagofTokens/version/cpp/948-BagofTokens_20260912_211311.cpp
@@ -0,0 +1,31 @@
+// Last updated: 12/09/2026, 21:13:11
+1class Solution {
+2public:
+3    int bagOfTokensScore(vector<int>& tokens, int power) {
+4        int n=tokens.size();
+5        int cnt=0;
+6        int ans=0;
+7
+8        sort(tokens.begin(),tokens.end());
+9
+10        int i=0,j=n-1;
+11
+12        while(i<=j){
+13            if(power>=tokens[i]){
+14                cnt++;
+15                power-=tokens[i];
+16                i++;
+17                ans=max(ans,cnt);
+18            }
+19            else if(cnt>0){
+20                power+=tokens[j];
+21                cnt--;
+22                j--;
+23                ans=max(ans,cnt);
+24            }
+25            else break;
+26        }
+27
+28        return ans;
+29    }
+30};
