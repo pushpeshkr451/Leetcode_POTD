6f6b0db9ab66b0cbcb8820a11dd5f1e14a7de5ba
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-09-14 12:32:08 +0530

    Create 836-RectangleOverlap

---
+++ 836-RectangleOverlap/version/cpp/836-RectangleOverlap_20260914_123207.cpp
@@ -0,0 +1,8 @@
+// Last updated: 14/09/2026, 12:32:07
+1class Solution {
+2public:
+3    bool isRectangleOverlap(vector<int>& r1, vector<int>& r2) {
+4         return r1[0] < r2[2] && r2[0] < r1[2] &&
+5               r1[1] < r2[3] && r2[1] < r1[3];
+6    }
+7};
