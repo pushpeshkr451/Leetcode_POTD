3b21802a946e884591ba13fcd3d7cc66de23fc37
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-09-19 15:32:40 +0530

    Create 1401-CircleandRectangleOverlapping

---
+++ 1401-CircleandRectangleOverlapping/version/cpp/1401-CircleandRectangleOverlapping_20260919_153239.cpp
@@ -0,0 +1,15 @@
+// Last updated: 19/09/2026, 15:32:39
+1class Solution {
+2public:
+3    bool checkOverlap(int radius, int xc, int yc, int x1, int y1, int x2, int y2) {
+4        int px = max(x1, min(xc, x2));
+5        int py = max(y1, min(yc, y2));
+6
+7        int disx = px - xc;
+8        int disy = py - yc;
+9
+10        int dis = disx*disx + disy*disy;
+11        
+12        return dis <= radius * radius;
+13    }
+14};
