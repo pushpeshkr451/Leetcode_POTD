6fb6b28c1298e842aaa26750bc0064b985e13787
Pushpesh Kumar <pushpeshkr451@gmail.com>
2026-09-24 19:12:24 +0530

    Create 3550-SmallestIndexWithDigitSumEqualtoIndex

---
+++ 3550-SmallestIndexWithDigitSumEqualtoIndex/version/cpp/3550-SmallestIndexWithDigitSumEqualtoIndex_20260924_191223.cpp
@@ -0,0 +1,15 @@
+// Last updated: 24/09/2026, 19:12:23
+1class Solution {
+2public:
+3    int smallestIndex(vector<int>& nums) {
+4        const int n=min(28, int(nums.size()));
+5        for(int i=0; i<n; i++){
+6            int x=nums[i], digitSum=0;
+7            for(; x>0; x/=10){
+8                digitSum+=x%10;
+9            }
+10            if (digitSum==i) return i;
+11        }
+12        return -1;
+13    }
+14};
